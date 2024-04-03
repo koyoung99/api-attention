f2e201c01a7bb38cd0873faadee531ad8b817683
User <User@DESKTOP-OE1LBHM>
2024-04-03 18:16:14 +0900

    attention

---
+++ src/main/java/com/javaex/ApiAttentionApplication.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAttentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAttentionApplication.class, args);
+	}
+
+}
